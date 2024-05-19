c6046cb9d89249630a94bc1bb167fa079cc97253
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-05-19 17:56:51 +0200

    help

--- PongSpiel/src/de/demoncore/audio/AudioManager.java
+++ PongSpiel/src/de/demoncore/audio/AudioManager.java
@@ -1,9 +1,10 @@
 package de.demoncore.audio;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 
-<<<<<<< Updated upstream
 public class AudioManager {
 
 	private float mainVolume;
@@ -12,13 +13,26 @@ public class AudioManager {
 	public void PlaySound(Clip clip, float volume) {
 		
 		
-		mainVolume *= volume;
+		mainVolume = mainVolume * volume;
 		
 	}
 	
 	public Clip AddAudioClip(String filePath) {
 		
-		
+		Clip in = null;
+
+	    try
+	    {
+	        AudioInputStream audioIn = AudioSystem.getAudioInputStream( getClass().getResource( filePath) );
+	        in = AudioSystem.getClip();
+	        in.open( audioIn );
+	    }
+	    catch( Exception e )
+	    {
+	        e.printStackTrace();
+	    }
+
+	    return in;
 		
 		
 		
@@ -26,42 +40,10 @@ public class AudioManager {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-=======
-
-public class AudioManager {
-
-	private float mainVolume;
-
-
-
-	public void PlaySound(Clip clip, float volume ) {
-
-		mainVolume *= volume;
-
-
-
-	}
-
-	public Clip AddAudioClip(String filePath ) {
-
-
-
-
-	}
-
-
-
-
->>>>>>> Stashed changes
 }
+	
+	
+	
+	
+	
+	
