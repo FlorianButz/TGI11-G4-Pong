b19ff55e828fe2554b711a18d11787962a057928
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-05-19 17:44:33 +0200

    idk
    stupid

---
+++ PongSpiel/src/de/demoncore/audio/AudioManager.java
@@ -0,0 +1,39 @@
+package de.demoncore.audio;
+
+import javax.sound.sampled.Clip;
+
+
+public class AudioManager {
+
+	private float mainVolume;
+	
+	
+	public void PlaySound(Clip clip, float volume) {
+		
+		
+		mainVolume *= volume;
+		
+	}
+	
+	public Clip AddAudioClip(String filePath) {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
