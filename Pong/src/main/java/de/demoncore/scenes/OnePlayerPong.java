586b828c81f41ceba1995d0ddab2c595d97172a2
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-06-05 09:40:04 +0200

    Player One scene

--- Pong/src/main/java/de/demoncore/scenes/OnePlayerPong.java
+++ Pong/src/main/java/de/demoncore/scenes/OnePlayerPong.java
@@ -22,19 +22,19 @@ public class OnePlayerPong extends BaseScene{
 		Player_One = new PongPlayer(0, 0);
 		AddObject(Player_One);
 	
-		Wand_Links = new BeweglichesRechteck(-960, 0, 25, 1100); // x, y, breite ,läne
+		Wand_Links = new BeweglichesRechteck(-960, 0, 25, 1150); // x, y, breite ,läne
 		AddObject(Wand_Links);
 		
 	
-		Wand_Rechts = new BeweglichesRechteck(960, 0, 25, 1100); // x, y, breite ,läne
+		Wand_Rechts = new BeweglichesRechteck(960, 0, 25, 1150); // x, y, breite ,läne
 		AddObject(Wand_Rechts);
 		
 		
-		Wand_Oben = new BeweglichesRechteck(0, 560, 1800, 25); // x, y, breite ,läne
+		Wand_Oben = new BeweglichesRechteck(0, 560, 1900, 25); // x, y, breite ,läne
 		AddObject(Wand_Oben);
 		
 		
-		Wand_Unten = new BeweglichesRechteck(0, -560, 1800, 25); // x, y, breite ,läne
+		Wand_Unten = new BeweglichesRechteck(0, -560, 1900, 25); // x, y, breite ,läne
 		AddObject(Wand_Unten);
 			
 
