778b79b8455f51063ce67c6dc2a7fca196ad1076
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-06-05 10:39:03 +0200

    Übersetzungssystem DONE

---
+++ Pong/src/main/java/de/demoncore/game/Language.java
@@ -0,0 +1,8 @@
+package de.demoncore.game;
+
+public enum Language {
+
+	Deutsch,
+	English
+	
+}
---
+++ Pong/src/main/java/de/demoncore/game/translation.java
@@ -0,0 +1,51 @@
+package de.demoncore.game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class translation {
+
+	static Language activeLanguage;
+	static Map<String, String> germanTranslation;
+	static Map<String, String> englishTranslation;
+	
+	public static void InitializeTranslation() {
+		
+		 activeLanguage = Language.Deutsch;
+		
+		germanTranslation = new HashMap<String, String>();
+		germanTranslation.put("test","Test Deutsch");
+		
+		englishTranslation = new HashMap<String, String>();
+		englishTranslation.put("test", "Test English!");
+		
+	
+	}
+	
+	public static String Get(String translationID) {
+		
+		switch (activeLanguage) {
+		case Deutsch:
+			return germanTranslation.get(translationID);
+			
+		case English:
+			return englishTranslation.get(translationID);
+	
+		default:
+			return englishTranslation.get(translationID);
+			
+		}
+		
+	}
+	
+	public static void ChangeLanguage(Language language) {
+		
+		activeLanguage = language;
+		
+		
+
+	}
+	
+	
+	
+}
