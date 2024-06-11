b198523df17dbc86c8aa6fce587f200b72fa312d
Hydra151 <phil151267@web.de>
2024-06-11 17:29:31 +0200

    Update PongPlayer.java

--- Pong/src/main/java/de/demoncore/gameObjects/PongPlayer.java
+++ Pong/src/main/java/de/demoncore/gameObjects/PongPlayer.java
@@ -1,12 +1,17 @@
 package de.demoncore.gameObjects;
 
 
+import de.demoncore.actions.KeyHandler;
 import de.demoncore.game.GameLogic;
+import de.demoncore.game.Logger;
+import de.demoncore.utils.Vector3;
 
 
 public class PongPlayer extends RigidBody {
 
 	public float playerAcceleration = 7.75f; // Speed
+	
+	public boolean isPlayer1 = true;
 	
 	public PongPlayer(int posX, int posY) {
 		super(posX, posY, 15, 135);
@@ -15,6 +20,14 @@ public class PongPlayer extends RigidBody {
 	@Override
 	public void Update() {
 		if(GameLogic.IsGamePaused()) return;
+		
+		Vector3 geschwindigkeit = Vector3.zero();
+		if (isPlayer1) {
+			geschwindigkeit = new Vector3(0,KeyHandler.playerInput1.y * playerAcceleration,0);
+		} else {
+			geschwindigkeit = new Vector3(0,KeyHandler.playerInput2.y * playerAcceleration,0);
+		}
+		AddForce(geschwindigkeit);
 		
 		super.Update();
 	}
