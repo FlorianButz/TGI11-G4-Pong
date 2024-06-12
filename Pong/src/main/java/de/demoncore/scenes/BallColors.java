6221a2d905f82074b57a830516ae306f1eb9ef82
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-06-12 11:04:02 +0200

    BallColor update

--- Pong/src/main/java/de/demoncore/scenes/BallColors.java
+++ Pong/src/main/java/de/demoncore/scenes/BallColors.java
@@ -1,12 +1,14 @@
 package de.demoncore.scenes;
 
+import java.awt.Color;
+
 import de.demoncore.game.GameObject;
 
 
 public class BallColors extends BaseScene {
 
 	GameObject ColorVorschau;
-	
+	GameObject Background;
 
 	
 	
@@ -17,11 +19,11 @@ public class BallColors extends BaseScene {
 		ColorVorschau = new GameObject(-500, -300, 100, 100);
 		AddObject(ColorVorschau);
 		
+		Background = new GameObject(500, 0, 1000, 2000);
+		AddObject(Background);
 		
 		
-		
-		
-		
+		Background.color = Color.darkGray;
 		
 		
 		
