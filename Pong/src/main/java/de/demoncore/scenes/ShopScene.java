2f0d8d2d9734e14ec9481702eb96cc0903004c5c
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-06-12 09:56:27 +0200

    shop "throne" erstellt
    soll das teil unter den optionen sein idk kann man entfernen falls es scheiße aussieht

--- Pong/src/main/java/de/demoncore/scenes/ShopScene.java
+++ Pong/src/main/java/de/demoncore/scenes/ShopScene.java
@@ -12,26 +12,50 @@ public class ShopScene extends BaseScene {
 	GameObject Base2;
 	GameObject Base3;
 	GameObject Base4;
-	
+	GameObject Throne1;
+	GameObject Throne2;
+	GameObject Throne3;
+	GameObject Throne4;
 	
 	@Override
 	public void initializeScene() {
 		
-		AddObject(new PauseMenu());
+		AddObject(new PauseMenu());	
 		
-		Base1 = new GameObject(-500, -250, 300, 300);
+		Base1 = new GameObject(-300, -250, 300, 300);
 		AddObject(Base1);
 		
-		Base1.color = Color.GRAY;
-		
-		Base2 = new GameObject(500, -250, 300, 300);
+		Base2 = new GameObject(300, -250, 300, 300);
 		AddObject(Base2);
 		
-		Base3 = new GameObject(-500, 250, 300, 300);
+		Base3 = new GameObject(-300, 250, 300, 300);
 		AddObject(Base3);
 		
-		Base4 = new GameObject(500, 250, 300, 300);
+		Base4 = new GameObject(300, 250, 300, 300);
 		AddObject(Base4);
+		
+		Base1.color = Color.DARK_GRAY;
+		Base2.color = Color.DARK_GRAY;
+		Base3.color = Color.DARK_GRAY;
+		Base4.color = Color.DARK_GRAY;
+		
+		Throne1 = new GameObject(-300, -250, 200, 200);
+		AddObject(Throne1);
+		
+		Throne2 = new GameObject(300, -250, 200, 200);
+		AddObject(Throne2);
+		
+		Throne3 = new GameObject(-300, 250, 200, 200);
+		AddObject(Throne3);
+		
+		Throne4 = new GameObject(300, 250, 200, 200);
+		AddObject(Throne4);
+		
+		Throne1.color = Color.LIGHT_GRAY;
+		Throne2.color = Color.lightGray;
+		Throne3.color = Color.lightGray;
+		Throne4.color = Color.LIGHT_GRAY;
+		
 		
 		
 		super.initializeScene();
