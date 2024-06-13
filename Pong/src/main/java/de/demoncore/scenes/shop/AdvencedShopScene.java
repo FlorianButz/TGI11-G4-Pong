b7692b09cba9b4f0a39041e3ff1e00ddc7784f4e
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-06-13 09:05:46 +0200

    Update AdvencedShopScene.java

--- Pong/src/main/java/de/demoncore/scenes/shop/AdvencedShopScene.java
+++ Pong/src/main/java/de/demoncore/scenes/shop/AdvencedShopScene.java
@@ -3,8 +3,14 @@ package de.demoncore.scenes.shop;
 import java.awt.Color;
 
 import de.demoncore.game.GameObject;
+import de.demoncore.game.Settings;
+import de.demoncore.game.Translation;
+import de.demoncore.gui.GUIAlignment;
+import de.demoncore.gui.GUIButton;
+import de.demoncore.gui.GUIButtonClickEvent;
 import de.demoncore.scenes.BaseScene;
-
+import de.demoncore.utils.Resources;
+import de.demoncore.gui.GUIButton;
 public class AdvencedShopScene extends BaseScene{
 
 
@@ -20,8 +26,25 @@ public class AdvencedShopScene extends BaseScene{
 		Background.color = Color.darkGray;
 
 		
+		GUIButton CreateBackButton() {
+			GUIButton back = new GUIButton(0, 0, 500, 50, Translation.get("settings.saveandback"), Resources.uiFont.deriveFont(25F), new GUIButtonClickEvent() {
+				@Override
+				public void ButtonClick() {
+					super.ButtonClick();
+					//HideMenu();
+					Settings.SaveAllSettings();
+				}
+			});
+			back.alignment = GUIAlignment.Center;
+			return back;
+		}
+		
+		
+	
+		
 		super.initializeScene();
 	}
+
 	
 	
 }
