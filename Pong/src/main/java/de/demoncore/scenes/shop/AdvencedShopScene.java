2f1fe6dbd306003917fc2623f7e8865812cf0af8
Marc1500 <110549198+Marc1500@users.noreply.github.com>
2024-06-26 07:39:16 +0200

    test shit

--- Pong/src/main/java/de/demoncore/scenes/shop/AdvencedShopScene.java
+++ Pong/src/main/java/de/demoncore/scenes/shop/AdvencedShopScene.java
@@ -11,20 +11,21 @@ import de.demoncore.gui.GUIAlignment;
 import de.demoncore.gui.GUIButton;
 import de.demoncore.gui.GUIButtonClickEvent;
 import de.demoncore.gui.GUIMenu;
+import de.demoncore.gui.GUIRectangle;
 import de.demoncore.scenes.BaseScene;
 import de.demoncore.utils.Resources;
 import de.demoncore.gui.GUIButton;
 public class AdvencedShopScene extends BaseScene{
 
-	GameObject Block;
+	GUIRectangle Block;
 	@Override
 	public void initializeScene() {
 			
 		
-		Block = new GameObject(0, 450, 2000, 250);
+		Block = new GUIRectangle(0, 450, 2000, 250, Color.DARK_GRAY);
 		addObject(Block);
 		
-		Block.color = Color.DARK_GRAY;
+
 		
 		GUIButton back = new GUIButton(0, 500, 600, 75, Translation.get("settings.saveandback"), Resources.uiFont.deriveFont(35F), new GUIButtonClickEvent() {
 			@Override
