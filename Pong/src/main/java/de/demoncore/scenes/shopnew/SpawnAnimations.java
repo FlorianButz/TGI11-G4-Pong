d32803b376e1a82930b38d5d215b3e4ae5d10af8
FlorianButz <75260616+FlorianButz@users.noreply.github.com>
2024-07-03 09:14:03 +0200

    Update

--- Pong/src/main/java/de/demoncore/scenes/shopnew/SpawnAnimations.java
+++ Pong/src/main/java/de/demoncore/scenes/shopnew/SpawnAnimations.java
@@ -2,7 +2,16 @@ package de.demoncore.scenes.shopnew;
 
 public enum SpawnAnimations {
 
-	None,
-	Latiku
+	None(0),
+	Latiku(75);
 	
+	private int numVal;
+
+	SpawnAnimations(int numVal) {
+		this.numVal = numVal;
+	}
+
+	public int getNumVal() {
+		return numVal;
+	}
 }
