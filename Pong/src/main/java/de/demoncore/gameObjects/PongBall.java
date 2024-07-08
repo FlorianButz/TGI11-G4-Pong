bd647125cff312d9b8ebe3595466ab4bcabcda1f
FlorianButz <75260616+FlorianButz@users.noreply.github.com>
2024-07-08 16:00:06 +0200

    Revert "ok gemacht hehe"
    This reverts commit 13c7bbba11fa7077274ee80dc6987d7bc156287f.

--- Pong/src/main/java/de/demoncore/gameObjects/PongBall.java
+++ Pong/src/main/java/de/demoncore/gameObjects/PongBall.java
@@ -177,9 +177,9 @@ public class PongBall extends GameObject {
 			speed = 30f;
 		}
 		
-		//position = position.add(velocity.multiply(speed)); 
-		// Wenn man sich ganz besonders fuehlt, Zeile mit dem replacen : //ok gemacht hehe
-		position = position.add(velocity.multiply(speed).add(new Vector3((float)Math.random() * 98 - 14f, (float)Math.random() * 98 - 14f)));
+		position = position.add(velocity.multiply(speed)); 
+		// Wenn man sich ganz besonders fuehlt, Zeile mit dem replacen :
+		//position = position.add(velocity.multiply(speed).add(new Vector3((float)Math.random() * 98 - 14f, (float)Math.random() * 98 - 14f)));
 
 		
 		if(player1.getBoundingBox().intersects(getBoundingBox()) || player2.getBoundingBox().intersects(getBoundingBox())) {
