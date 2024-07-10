103dc27ef612b3f2d477cff7e7e7a7851d94274a
Timinator07 <hilzingertim@gmx.de>
2024-07-10 09:15:21 +0200

    Update GuiCredidtsMenu.java

--- Pong/src/main/java/de/demoncore/gui/GuiCredidtsMenu.java
+++ Pong/src/main/java/de/demoncore/gui/GuiCredidtsMenu.java
@@ -4,17 +4,28 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.demoncore.game.GameLogic;
 import de.demoncore.game.Translation;
+import de.demoncore.utils.GameMath;
 import de.demoncore.utils.Resources;
 
 public class GuiCredidtsMenu extends GUIMenu{
+	
+	
+	private GUITextArea guiText;
+
+	public static Color regenbogen() {
+		return GameMath.lerpColor(
+				GameMath.lerpColor(Color.magenta, Color.green, (float) Math.sin(GameLogic.getInstance().getGameTime() *2)),
+				Color.cyan, (float) Math.sin(GameLogic.getInstance().getGameTime() * 1));
+	}
 	
 	@Override
 	protected List<GUIObject> addMenuContent() {
 
 		List<GUIObject> Objects = new ArrayList<GUIObject>();
 		
-		GUITextArea guiText = new GUITextArea(0, -650, Translation.literal("\r\n"
+		guiText = new GUITextArea(0, -650, Translation.literal("\r\n"
 				+ "\bGrafikdesigner\r\n"
 				+ "\r\n"
 				+ "Jonathan Kugler\r\n"
@@ -29,7 +40,7 @@ public class GuiCredidtsMenu extends GUIMenu{
 				+ "\r\n"
 				+ "\bMusik\r\n"
 				+ "\r\n"
-				+ "Florian Butz"), Resources.uiFont.deriveFont(40f),Color.cyan);
+				+ "Florian Butz"), Resources.uiFont.deriveFont(40f),regenbogen());
 		guiText.alignment = GUIAlignment.Center;
 		guiText.SetTextAlignment(TextAlignment.Center);
 		Objects.add(guiText);
@@ -37,4 +48,11 @@ public class GuiCredidtsMenu extends GUIMenu{
 		return Objects;
 	}
 
+	@Override
+	public void update() {
+		super.update();
+
+		if(guiText != null)
+			guiText.color = regenbogen();
+	}
 }
